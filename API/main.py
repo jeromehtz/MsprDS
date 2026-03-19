8455ae148128a74e6d3630f4ab7f7de7e21d8d6f
Tari Masinisa <masitari6@gmail.com>
2026-03-20 00:16:52 +0100

    pull data

--- API/main.py
+++ API/main.py
@@ -32,12 +32,20 @@ class trajetsFerroviaires:
         
         return [dict(row) for row in rows]
 
-    def get_trajets_interpays(self) -> List[dict]:
+    def get_donneesFerroviaires(self) -> List[dict]:
         self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
         self.cursor.execute("SELECT * FROM trajetsInterPays")
         rows = self.cursor.fetchall()
         
+        return [dict(row) for row in rows]
+
+    def get_CO2_data(self) -> List[dict]:
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("SELECT * FROM carbon_dioxide")
+        rows = self.cursor.fetchall()
+
         return [dict(row) for row in rows]
 
     def calculer_itineraire(self, depart: str, arrivee: str) -> List[dict]:
@@ -60,7 +68,7 @@ class trajetsFerroviaires:
             return {"message": "Erreur lors de la connexion à la base de données."}
         return [dict(row) for row in result]    
 
-trajets_interpays = trajetsFerroviaires("../bdd/trajetsFerroviairesBDD.db")
+donneesFerroviaires = trajetsFerroviaires("../bdd/trajetsFerroviairesBDD.db")
 
 
 
@@ -74,18 +82,21 @@ def accueil():
 
 @app.get("/trajetsInterPays", response_model=List[dict])
 def obetnir_trajets_inter_Pays():
-    return trajets_interpays.get_trajets_interpays()
+    return donneesFerroviaires.get_donneesFerroviaires()
 
 @app.get("/trajets")
 def obetnir_trajets():
     trajets = trajetsFerroviaires("../bdd/trajetsFerroviairesBDD.db").get_trajets()
     return trajets
 
+@app.get("/donnesCO2", response_model=List[dict])
+def obetnir_donnees_CO2():
+    return donneesFerroviaires.get_CO2_data()
+
 @app.get("/itineraire")
 def calculer_itineraire(depart: str, arrivee: str):
-    itineraire = trajets_interpays.calculer_itineraire(depart, arrivee)
+    itineraire = donneesFerroviaires.calculer_itineraire(depart, arrivee)
     return {"itineraire": itineraire}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
