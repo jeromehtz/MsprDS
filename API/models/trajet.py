b9e3478960db0be35674f19739f79d867695fda0
jeromehtz <rejome.htz@gmail.com>
2026-06-24 19:28:04 +0200

    feat - ajout du grafana

--- API/models/trajet.py
+++ API/models/trajet.py
@@ -1,15 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 class Trajet(Base):
     __tablename__ = "trajets"
 
-    id = Column(
-        Integer,
-        primary_key=True,
-        index=True
-    )
-
-    departure_city = Column(String)
-    arrival_city = Column(String)
-    duration = Column(String)
+    year = Column(Integer, primary_key=True)
+    origin_station_name = Column(String, primary_key=True)
+    destination_station_name = Column(String, primary_key=True)
+    origin_city = Column(String, primary_key=True)
+    destination_city = Column(String, primary_key=True)
+    origin_region = Column(String, primary_key=True)
+    destination_region = Column(String, primary_key=True)
+    passengers_millions = Column("Passengers_millions", Float)
+    type = Column("Type", String)
+    source = Column(String)
